1f4a9768da4b0a959eeaa42a154e571ba6a120b1
RcnonS <ratchanon.somprasong@gmail.com>
2024-05-22 13:56:15 +0700

    question-3

--- question-3.js
+++ question-3.js
@@ -1,3 +1,15 @@
 // Question #3
-let userPassword = "";
+let userPassword = "abcde";
 // เริ่มเขียนโค้ดตรงนี้
+
+function checkPasswordStrength(password) {
+  if (password.length < 6) {
+    return "Weak";
+  } else if (password.length >= 6 && password.length < 10) {
+    return "Medium";
+  } else if (password.length >= 10) {
+    return "Strong";
+  }
+}
+
+console.log(checkPasswordStrength(userPassword));
