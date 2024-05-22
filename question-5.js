35278fcaf51590aa15376072c896326d5acc8ada
RcnonS <ratchanon.somprasong@gmail.com>
2024-05-22 14:40:40 +0700

    question-5

--- question-5.js
+++ question-5.js
@@ -19,3 +19,20 @@ const products = [
 
 const promotionCode = "";
 // เริ่มเขียนโค้ดตรงนี้
+
+function calculateTotalPrice(item, discountCode) {
+  let totalPrice = 0;
+  for (let i = 0; i < item.length; i++) {
+    let result = item[i].price * item[i].quantity;
+    totalPrice += result;
+  }
+  if (discountCode === "SALE20") {
+    return totalPrice * 0.8;
+  } else if (discountCode === "SALE50") {
+    return totalPrice * 0.5;
+  } else {
+    return totalPrice;
+  }
+}
+
+console.log(calculateTotalPrice(products, promotionCode));
